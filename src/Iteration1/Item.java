4c72a511867763a2f69af55a493231fb5ceccb12
Shagun-10 <shagunp1@iastate.edu>
2025-11-10 17:52:40 -0600

    Add files via upload

---
+++ src/Iteration1/Item.java
@@ -0,0 +1,33 @@
+package Iteration1;
+
+public class Item {
+    private String itemID;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Item(String itemID, String name, double price, int quantity) {
+        this.itemID = itemID;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public void updateStock(int newQuantity) {
+        this.quantity = newQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return itemID + "," + name + "," + price + "," + quantity;
+    }
+
+    public static Item fromString(String line) {
+        String[] parts = line.split(",");
+        return new Item(parts[0], parts[1], Double.parseDouble(parts[2]), Integer.parseInt(parts[3]));
+    }
+
+    public String getItemID() { return itemID; }
+    public int getQuantity() { return quantity; }
+}
+
