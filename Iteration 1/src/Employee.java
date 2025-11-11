2c16023f872b9869269019cc9aa6391dbd14f9ec
Rei Aoki <rayaoki0709@gmail.com>
2025-11-10 18:10:57 -0600

    Removing metadata

--- Iteration 1/src/Employee.java
+++ Iteration 1/src/Employee.java
@@ -1,5 +1,17 @@
-
 public abstract class Employee {
+	private String name;
 	private double salary;
-	private ShiftWork shift;
+	private String role;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getSalaryInfo() {
+		return salary;
+	}
+	
+	public String getRole() {
+		return role;
+	}
 }
